c54dc715f51da4346a6055b23b2b07bd39cbfeb1
Jack Spencer <112727830+Jxck17x@users.noreply.github.com>
2022-09-12 13:47:48 +0530

    Add files via upload

---
+++ 192110743 day 2 experiment 2.py
@@ -0,0 +1,16 @@
+def area(n,len):
+      a=0
+      for i in range(len):
+        for j in range(i+1,len):
+              a=max(a,min(n[j],n[i]*(j-i))
+        return a
+i=int(input("enter the number of elements"))
+ist=[]
+for i in range(0,i);
+      n=int(input("enter the elements:"))
+      ist.append
+if(i<=0)or(n<=0)):
+    print("invald")
+else:
+      len1=len
+      print(area(n,len1))
